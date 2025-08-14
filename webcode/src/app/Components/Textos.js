a038c8c35b95177d9e9b028c00e2f79b464afe3d
Ramon-24 <ramongomesbs24@gmail.com>
2025-08-13 21:04:29 -0300

    testear

--- webcode/src/app/Components/Textos.js
+++ webcode/src/app/Components/Textos.js
@@ -36,7 +36,7 @@ export const Textos = {
                 Tecnologia: "Tecnologia de ponta"
             }
         },
-        servicos:{
+        servicos: {
             titulo: "Serviços Prestados",
             cards: [
                 {
@@ -69,5 +69,163 @@ export const Textos = {
             ],
             botao: "Ainda tenho dúvidas?"
         },
+    },
+
+
+
+
+
+
+
+
+
+
+
+    en: {
+        nav: {
+            // imagem: "/logo.png",
+            nome: "WebCode",
+            links: {
+                inicio: "Home",
+                sobre: "About",
+                servicos: "Services",
+                projetos: "Projects",
+                contato: "Contact"
+            }
+        },
+        hero: {
+            fraseP1: "We connect your ",
+            fraseP2: " idea to the ",
+            fraseP3: " digital world ",
+            descricao: "With affordable, personalized solutions designed to make your brand grow with impact, strategy, and a true online presence.",
+            orcamento: "Request a quote",
+            trabalhos: "See our work"
+        },
+        sobre: {
+            titulo1: "What can",
+            titulo2: " webcode",
+            titulo3: " provide you?",
+            descricao1: "Webcode can provide you with fast, secure, and personalized technological solutions, ensuring that your business is always online and performing at its best.",
+            descricao2: "With close service and specialized support, we take care of every detail so you have more time to focus on what really matters: the growth of your project.",
+            qualificacao: {
+                Suporte: "Fast and effective support",
+                Soluções: "Tailored solutions",
+                Segurança: "Security and stability",
+                Atendimento: "Humanized service",
+                Desempenho: "Optimized performance",
+                Acompanhamento: "Customer follow-up",
+                Profissionais: "Qualified professionals",
+                Tecnologia: "Cutting-edge technology"
+            }
+        },
+        servicos: {
+            titulo: "Services Provided",
+            cards: [
+                {
+                    // imagem: "/",
+                    titulo: "Web Development",
+                    descricao: "Creation of personalized websites and systems, optimized for SEO and responsive."
+                },
+                {
+                    // imagem: "/",
+                    titulo: "Website Hosting",
+                    descricao: "Fast, secure hosting with specialized technical support."
+                },
+            ]
+        },
+        perguntasFrequentes: {
+            titulo: "Have any questions?",
+            perguntas: [
+                {
+                    pergunta: "How does the development process work?",
+                    resposta: "Our process is agile and collaborative, involving you in every step to ensure the final result meets your expectations."
+                },
+                {
+                    pergunta: "What technologies do you use?",
+                    resposta: "We use the latest technologies suitable for your project, ensuring quality and performance."
+                },
+                {
+                    pergunta: "Do you offer technical support?",
+                    resposta: "Yes, we offer ongoing technical support to ensure your site or system runs smoothly."
+                }
+            ],
+            botao: "I still have questions?"
+        },
+    },
+
+
+
+    
+
+    es: {
+        nav: {
+            // imagem: "/logo.png",
+            nome: "WebCode",
+            links: {
+                inicio: "Inicio",
+                sobre: "Sobre",
+                servicos: "Servicios",
+                projetos: "Proyectos",
+                contato: "Contacto"
+            }
+        },
+        hero: {
+            fraseP1: "Conectamos tu ",
+            fraseP2: " idea al ",
+            fraseP3: " mundo digital ",
+            descricao: "Con soluciones accesibles, personalizadas y pensadas para hacer crecer tu marca con impacto, estrategia y presencia online de verdad.",
+            orcamento: "Solicita un presupuesto",
+            trabalhos: "Ver nuestro trabajo"
+        },
+        sobre: {
+            titulo1: "¿Qué puede ofrecerte la",
+            titulo2: " webcode?",
+            titulo3: "?",
+            descricao1: "Webcode puede ofrecerte soluciones tecnológicas rápidas, seguras y personalizadas, asegurando que tu negocio esté siempre en línea y funcionando al mejor rendimiento.",
+            descricao2: "Con un servicio cercano y soporte especializado, cuidamos cada detalle para que tengas más tiempo para enfocarte en lo que realmente importa: el crecimiento de tu proyecto.",
+            qualificacao: {
+                Suporte: "Soporte rápido y eficaz",
+                Soluciones: "Soluciones a medida",
+                Seguridad: "Seguridad y estabilidad",
+                Atendimento: "Atención humanizada",
+                Desempenho: "Rendimiento optimizado",
+                Acompanhamento: "Seguimiento al cliente",
+                Profissionais: "Profesionales cualificados",
+                Tecnologia: "Tecnología de vanguardia"
+            }
+        },
+        servicos: {
+            titulo: "Servicios Prestados",
+            cards: [
+                {
+                    // imagem:"/",
+                    titulo: "Desarrollo Web",
+                    descricao: "Creación de sitios web y sistemas personalizados, optimizados para SEO y responsivos."
+                },
+                {
+                    // imagem:"/",
+                    titulo: "Alojamiento de Sitios Web",
+                    descricao: "Alojamiento rápido, seguro y con soporte técnico especializado."
+                },
+            ]
+        },
+        perguntasFrequentes: {
+            titulo: "¿Tienes alguna duda?",
+            perguntas: [
+                {
+                    pergunta: "¿Cómo funciona el proceso de desarrollo?",
+                    resposta: "Nuestro proceso es ágil y colaborativo, involucrándote en cada etapa para garantizar que el resultado final cumpla con tus expectativas."
+                },
+                {
+                    pergunta: "¿Qué tecnologías utilizan?",
+                    resposta: "Utilizamos las tecnologías más recientes y adecuadas para tu proyecto, asegurando calidad y rendimiento."
+                },
+                {
+                    pergunta: "¿Ofrecen soporte técnico?",
+                    resposta: "Sí, ofrecemos soporte técnico continuo para garantizar que tu sitio o sistema funcione perfectamente."
+                }
+            ],
+            botao: "Aún tengo dudas"
+        }
     }
 };
